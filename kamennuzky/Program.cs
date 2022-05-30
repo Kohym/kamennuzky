de5e8945f82595075f402cb479d05d2667570791
Marek Kohout <marek.kohout@educhem.cz>
2022-05-30 15:16:45 +0200

    added lore

--- kamennuzky/Program.cs
+++ kamennuzky/Program.cs
@@ -1,25 +1,96 @@
-﻿Random random = new Random();
+﻿using kamennuzky;
+#region proměnécreate
+Random random = new Random();
 int hracwin = 0;
 int botwin = 0;
 string bot = "";
 string hrac = "";
-
-while (hracwin != 5 & botwin != 5)
+bool lore1 = false;
+bool lore2 = false;
+bool lore3 = false;
+#endregion proměnécreate
+#region gamestart
+Console.BackgroundColor = ConsoleColor.Red;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine("pro optimální expirience hraj v maximalizovaném okně");
+Console.BackgroundColor = ConsoleColor.Green;
+Console.WriteLine("zmáčkni cokoliv pro start hry");
+Console.ResetColor();
+Console.ReadKey();
+Console.Clear();
+#endregion gamestart
+Console.WriteLine("Chceš ve hře Lore? y/n");
+string lore = Console.ReadLine();
+#region lore1
+if (lore == "y" || lore == "Y")
 {
     Console.Clear();
+    lore = "y";
+    Console.WriteLine("Lore:");
+    Console.Write("V roce 1987 se v fredyho kámen restauraci zaměstnanec jménem ");
+    Console.ForegroundColor= ConsoleColor.Blue;
+    Console.Write("Wiliam Afton ");
     Console.ForegroundColor = ConsoleColor.White;
+    Console.Write("zahrál nůžky na 5 dětí které zahráli papír a jejich mrtvá těla nacpal do animatroniků kteří v restauraci chilovali");
+}
+else
+{
+    Console.Clear();
+    Console.WriteLine("nechceš lore tak užívej");
+}
+#endregion lore1
+Console.ReadKey();
+Console.Clear();
+
+#region game
+while (hracwin != 5 & botwin != 5)
+{
+    #region lore2
+    winhrac trida = new winhrac();
+    if ((hracwin == 0 || botwin == 0) & lore == "y" & lore1 == false)
+    {
+        lore1 = true;
+        Console.Write("Jsi ");
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write("Wiliam Afton ");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("který po incident nazvaným: šmik 87 musí bojovat proti policii aby nenašli těla");
+        Console.ReadKey();
+    }
+    else if ((hracwin == 2 || botwin == 2) &  lore == "y" & lore2 == false)
+    {
+        Console.Clear();
+        lore2 = true;
+        Console.WriteLine("zatím se ti to daří ale už tě to přestává bavit");
+        Console.ReadKey();
+    }
+    else if ((hracwin == 2 || botwin == 2) & lore == "y" & lore3 == false) 
+    {
+        Console.Clear();
+        lore3 = true;
+        Console.WriteLine("Zvládl jsi to až sem ale už tě nebaví se schovávat tak jsi šel do již zavřené budovy kde se stal šmik 87 a chtěl jsi zničit ty animatroniky teré ovládly duš šmiknutých dětí");
+        Console.ReadKey();
+    }
+    #endregion lore2
+    Console.Clear();
+    Console.ResetColor();
+    #region gamestartingtext
     Console.ForegroundColor = ConsoleColor.Blue;
     Console.WriteLine($"más {hracwin} bodů");
     Console.ForegroundColor = ConsoleColor.Red;
     Console.WriteLine($"protihráč má {botwin} bodů");
-    Console.ForegroundColor = ConsoleColor.White;
+    Console.ResetColor();
     Console.WriteLine("Zadej: kámen, papír nebo nůžky");
+    #endregion gamestartingtext
     hrac = Console.ReadLine();
+    #region umispsat?
     if (hrac != "kámen" & hrac != "papír" & hrac != "nůžky")
     {
         Console.WriteLine(value: "neumíš psát debile");
         Console.ReadKey();
     }
+    #endregion umispsat?
+    #region acualgame
     else
     {
         switch (random.Next(1, 4))
@@ -38,7 +109,7 @@ while (hracwin != 5 & botwin != 5)
         Console.WriteLine($"zadal jsi: {hrac}");
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine($"protihráč zadal: {bot}");
-        Console.ForegroundColor = ConsoleColor.White;
+        Console.ResetColor();
         switch (hrac)
         {
             case "kámen":
@@ -48,25 +119,19 @@ while (hracwin != 5 & botwin != 5)
                 }
                 else if (bot == "papír")
                 {
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.WriteLine("prohrál jsi");
-                    Console.BackgroundColor = ConsoleColor.Black;
+                    trida.WinBot();
                     botwin++;
                 }
                 else
                 {
-                    Console.BackgroundColor = ConsoleColor.Blue;
-                    Console.WriteLine("Vyhral jsi");
-                    Console.BackgroundColor = ConsoleColor.Black;
+                    trida.WinHrac();
                     hracwin++;
                 }
                 break;
             case "papír":
                 if (bot == "kámen")
                 {
-                    Console.BackgroundColor = ConsoleColor.Blue;
-                    Console.WriteLine("Vyhral jsi");
-                    Console.BackgroundColor = ConsoleColor.Black;
+                    trida.WinHrac();
                     hracwin++;
                 }
                 else if (bot == "papír")
@@ -75,25 +140,19 @@ while (hracwin != 5 & botwin != 5)
                 }
                 else
                 {
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.WriteLine("prohrál jsi");
-                    Console.BackgroundColor = ConsoleColor.Black;
+                    trida.WinBot();
                     botwin++;
                 }
                 break;
             case "nůžky":
                 if (bot == "kámen")
                 {
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.WriteLine("prohrál jsi");
-                    Console.BackgroundColor = ConsoleColor.Black;
+                    trida.WinBot();
                     botwin++;
                 }
                 else if (bot == "papír")
                 {
-                    Console.BackgroundColor = ConsoleColor.Blue;
-                    Console.WriteLine("Vyhral jsi");
-                    Console.BackgroundColor = ConsoleColor.Black;
+                    trida.WinHrac();
                     hracwin++;
                 }
                 else
@@ -104,17 +163,31 @@ while (hracwin != 5 & botwin != 5)
         }
         Console.ReadKey();
     }
+    #endregion acualgame
 }
+#endregion game
 Console.Clear();
-if (botwin == 5)
+#region end
+if (botwin == 5 & lore == "y" || lore == "Y")
+{
+    Console.WriteLine("Přišl jsi tvaří v tvář s kámen animatroniky a zahrál jsi na ně svůj nůžky move ale byl jsi kokot protže oni zahráli kámen (jsou kamení) a zabili tě");
+    Console.WriteLine("The end");
+}
+else if (hracwin == 5 & lore == "y" || lore == "Y")
+{
+    Console.WriteLine("Přišl jsi tvaří v tvář s kámen animatroniky a zahrál jsi na ně papír a tím jsi je porazil");
+    Console.WriteLine("The end");
+}
+else if (botwin == 5 & lore != "y")
 {
     Console.ForegroundColor = ConsoleColor.Red;
     Console.WriteLine($"nedal jsi to ale měl jsi {hracwin} body :(");
     Console.ForegroundColor = ConsoleColor.White;
 }
-else if (hracwin == 5)
+else if (hracwin == 5 & lore != "y")
 {
     Console.ForegroundColor = ConsoleColor.Blue;
     Console.WriteLine($"GG vyhrál jsi a soupeř měl jenom {botwin} body! :)");
     Console.ForegroundColor = ConsoleColor.White;
 }
+#endregion end
---
+++ kamennuzky/winhrac.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kamennuzky
+{
+    internal class winhrac
+    {
+        public void WinHrac()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Máš bod");
+            Console.ResetColor();
+        }
+        public void WinBot()
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("nepřítel má bod");
+            Console.ResetColor();
+        }
+    }   
+}
